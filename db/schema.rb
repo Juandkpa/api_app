b7a529ce5e0081d3821b0816cf6493d0dc957a01
Juan Parra <juandkpa@gmail.com>
2018-10-05 17:32:16 -0500

    authorization

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_10_05_053439) do
+ActiveRecord::Schema.define(version: 2018_10_05_215121) do
 
   create_table "authors", force: :cascade do |t|
     t.string "code"
@@ -34,6 +34,17 @@ ActiveRecord::Schema.define(version: 2018_10_05_053439) do
     t.string "type_p"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.string "username"
+    t.string "password_digest"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["username"], name: "index_users_on_username", unique: true
   end
 
 end
